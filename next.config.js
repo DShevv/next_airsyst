8cc31c15514a538227007b82f4c8901945ddbeb8
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-10-12 11:44:42 +0300

    add user cabinet page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //output: "export",
+  rewrites: async () => [
+    {
+      source: "/cab",
+      destination: "/cab.html",
+    },
+  ],
   images: {
     remotePatterns: [
       {
