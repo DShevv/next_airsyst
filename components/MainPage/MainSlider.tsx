6f9f96dc059deedf489e894a7d0be7360a0b1c4c
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-10-15 18:01:01 +0300

    add actual data to main slider

--- components/MainPage/MainSlider.tsx
+++ components/MainPage/MainSlider.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
-import testPic from "../../public/img/images/Group 185.png";
+import oxygen from "@/public/img/images/Screenshot_2.jpg";
+import geno from "@/public/img/images/Group_199.png";
+import vozdu from "@/public/img/images/1603360871_11.png";
+import vozda from "@/public/img/images/Group_197.png";
 import Image from "next/image";
 
 export default function MainSlider() {
@@ -23,16 +26,28 @@ export default function MainSlider() {
         className="hero-slider swiper"
       >
         <SwiperSlide className="hero-slide swiper-slide">
-          <h2 className="title">Воздуходувки</h2>
+          <h2 className="title">Подбор азотных и кислородных станций</h2>
           <h3 className="text">
-            Роторные и вихревые воздуходувки AIRSYST, производятся в Турецкой
-            Республике из европейских комплектующих. Высокая надежность и
-            быстрая поставка.
+            Опытные специалисты AIRSYS помогут Вам подобрать оптимальное решение
+            по обеспечению предприятия газообразным азотом и кислородом. Если Вы
+            покупаете жидкий азот или кислород в баллонах , мы рассчитаем и
+            поставим под ключ все необходимое оборудование.
           </h3>
           <a href="{{slider.link}}" className="btn">
             Узнать больше
           </a>
-          <Image src={testPic} alt="" width={260} height={350} />
+          <Image src={oxygen} alt="" width={260} height={350} />
+        </SwiperSlide>
+        <SwiperSlide className="hero-slide swiper-slide">
+          <h2 className="title">Генераторы кислорода</h2>
+          <h3 className="text">
+            Генераторы кислорода AIRSYST серии GO производительностью до 390
+            м3/ч. Чистотой до 99%
+          </h3>
+          <a href="{{slider.link}}" className="btn">
+            Узнать больше
+          </a>
+          <Image src={geno} alt="" width={260} height={350} />
         </SwiperSlide>
         <SwiperSlide className="hero-slide swiper-slide">
           <h2 className="title">Воздуходувки</h2>
@@ -44,31 +59,18 @@ export default function MainSlider() {
           <a href="{{slider.link}}" className="btn">
             Узнать больше
           </a>
-          <Image src={testPic} alt="" width={260} height={350} />
+          <Image src={vozdu} alt="" width={260} height={350} />
         </SwiperSlide>
         <SwiperSlide className="hero-slide swiper-slide">
-          <h2 className="title">Воздуходувки</h2>
+          <h2 className="title">Генераторы азота</h2>
           <h3 className="text">
-            Роторные и вихревые воздуходувки AIRSYST, производятся в Турецкой
-            Республике из европейских комплектующих. Высокая надежность и
-            быстрая поставка.
+            Генераторы азота AIRSYST серии GN производительностью до 2750 м3/ч.
+            Чистотой до 99,9999%
           </h3>
           <a href="{{slider.link}}" className="btn">
             Узнать больше
           </a>
-          <Image src={testPic} alt="" width={260} height={350} />
-        </SwiperSlide>
-        <SwiperSlide className="hero-slide swiper-slide">
-          <h2 className="title">Воздуходувки</h2>
-          <h3 className="text">
-            Роторные и вихревые воздуходувки AIRSYST, производятся в Турецкой
-            Республике из европейских комплектующих. Высокая надежность и
-            быстрая поставка.
-          </h3>
-          <a href="{{slider.link}}" className="btn">
-            Узнать больше
-          </a>
-          <Image src={testPic} alt="" width={490} height={444} />
+          <Image src={vozda} alt="" width={490} height={444} />
         </SwiperSlide>
       </Swiper>
     </>
