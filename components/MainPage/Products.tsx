aa90110b5c26a9165c124fb710653bcb0f0e76bd
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-10-16 10:35:27 +0300

    add companies to products page

--- components/MainPage/Products.tsx
+++ components/MainPage/Products.tsx
@@ -3,8 +3,27 @@ import obrImage from "../../public/img/images/main-eq.png";
 import sevice from "../../public/img/images/bid-2.png";
 import parts from "../../public/img/images/bid-1.png";
 import airsyst from "../../public/img/images/airsyst.svg";
+import comp from "@/public/img/images/comp.svg";
+import comp1 from "@/public/img/images/comp1.svg";
+import comp2 from "@/public/img/images/comp3.svg";
+import comp3 from "@/public/img/images/comp4.svg";
+import comp4 from "@/public/img/images/comp5.svg";
+import comp5 from "@/public/img/images/comp6.svg";
+import comp6 from "@/public/img/images/comp7.svg";
+import comp7 from "@/public/img/images/comp2.svg";
 
-const companiess = [airsyst, airsyst, airsyst, airsyst, airsyst, airsyst];
+const airsysts = [airsyst, airsyst, airsyst, airsyst, airsyst, airsyst];
+const companies = [
+  comp,
+  comp1,
+  comp2,
+  comp3,
+  comp4,
+  comp5,
+  comp6,
+  comp7,
+  airsyst,
+];
 
 export default function Products() {
   return (
@@ -14,19 +33,19 @@ export default function Products() {
           title="Оборудование"
           image={obrImage}
           href="/oborudovanie"
-          companies={companiess}
+          companies={airsysts}
         />
         <ProductItem
           title="Сервис и обслуживание"
           image={sevice}
           href="/service-center"
-          companies={companiess}
+          companies={companies}
         />
         <ProductItem
           title="ЗАПЧАСТИ И РАСХОДНЫЕ МАТЕРИАЛЫ ДЛЯ КОМПРЕССОРОВ"
           image={parts}
           href="/parts"
-          companies={companiess}
+          companies={companies}
         />
       </div>
     </section>
