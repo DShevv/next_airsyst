cce2b61e3677b974cb71837ef04bdf7fdc20f426
Dmitry Shevtsov <dshevtsov20@gmail.com>
2023-12-17 17:42:36 +0300

    fix cab link

--- components/Cab.tsx
+++ components/Cab.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Cab() {
   return (
-    <a className="mail cab" href="/cab">
+    <a className="mail cab" href="/cab/">
       <Image width={25} height={19} src={Icon} alt="" />
       <span>Личный кабинет</span>
     </a>
